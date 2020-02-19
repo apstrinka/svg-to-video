283e6a93e91368d4f0697274101ef3790afbb93c
apstrinka <apstrinka@gmail.com>
2020-02-18 21:29:32 -0800

    Add colors, begin time lists and different clock formats

--- svg-to-video.py
+++ svg-to-video.py
@@ -5,6 +5,16 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 
+class Color:
+	def __init__(self, red, green, blue, alpha):
+		self.red = red
+		self.green = green
+		self.blue = blue
+		self.alpha = alpha
+	
+	def __str__(self):
+		return "rgba(" + str(self.red) + "," + str(self.green) + "," + str(self.blue) + "," + str(self.alpha) + ")"
+
 def main():
 	if len(sys.argv) < 4:
 		print("Usage: python svg-to-video.py (filename) (duration in seconds) (framerate in frames/second)")
@@ -59,9 +69,9 @@ def processElement(element, time):
 
 def processAnimateTag(element, tag, time):
 	attributeName = tag.attrib['attributeName']
-	begin = 0
+	beginList = [0]
 	if 'begin' in tag.attrib:
-		begin = parseClockValue(tag.attrib['begin'])
+		beginList = parseBeginValue(tag.attrib['begin'])
 	dur = parseClockValue(tag.attrib['dur'])
 	repeatDur = dur
 	if 'repeatDur' in tag.attrib:
@@ -75,8 +85,12 @@ def processAnimateTag(element, tag, time):
 		else:
 			repeatDur = dur*float(tag.attrib['repeatCount'])
 	
-	if time < begin:
+	nonfutureBeginList = [b for b in beginList if b <= time]
+	if len(nonfutureBeginList) == 0:
 		return
+	
+	begin = max(nonfutureBeginList)
+	
 	if repeatDur is not None and time > begin + repeatDur:
 		if 'fill' in tag.attrib and tag.attrib['fill'] == 'freeze':
 			element.attrib[attributeName] = tag.attrib['to']
@@ -88,8 +102,11 @@ def processAnimateTag(element, tag, time):
 	while t > dur:
 		t = t - dur
 	t = t/dur
-	interpolated = interpolateValueArray(fromArr, toArr, t)
-	element.attrib[attributeName] = ' '.join(interpolated)
+	interpolated = interpolate(fromArr, toArr, t)
+	if isinstance(interpolated, Color):
+		element.attrib[attributeName] = str(interpolated)
+	else:
+		element.attrib[attributeName] = ' '.join(interpolated)
 
 def processSetTag(element, tag, time):
 	attributeName = tag.attrib['attributeName']
@@ -141,12 +158,61 @@ def processAnimateTransformTag(element, tag, time):
 	element.attrib[attributeName] = type + '(' + ' '.join(interpolated) + ')'
 
 def parseValue(value):
+	if value.startswith("rgba("):
+		subvalues = value[4:-1].replace(',', ' ').split()
+		return Color(float(subvalues[0]), float(subvalues[1]), float(subvalues[2]), float(subvalues[3]))
+	if value.startswith("rgb("):
+		subvalues = value[4:-1].replace(',', ' ').split()
+		print(subvalues)
+		return Color(float(subvalues[0]), float(subvalues[1]), float(subvalues[2]), 100.0)
+	if value.startswith("#"):
+		if len(value) == 4:
+			return Color(float(int(value[1], 16)*16), float(int(value[2], 16)*16), float(int(value[3], 16)*16), 100.0)
+		if len(value) == 7:
+			return Color(float(int(value[1:3], 16)), float(int(value[3:5], 16)), float(int(value[5:7], 16)), 100.0)
+		if len(value) == 9:
+			return Color(float(int(value[3:5], 16)), float(int(value[5:7], 16)), float(int(value[7:9], 16)), float(int(value[3:5], 16))*100/255)
 	subvalues = value.replace(',', ' ').split()
 	return [float(i) for i in subvalues]
 
+def parseBeginValue(value):
+	values = value.split(';')
+	return [parseClockValue(v) for v in values]
+
 def parseClockValue(value):
-	#Currently only time values in the form "12.3s" supported
-	return float(value[:-1])
+	components = value.split(':')
+	if len(components) == 3:
+		hours = int(components[0])
+		minutes = int(components[1])
+		seconds = float(components[2])
+		return 3600*hours + 60*minutes + seconds
+	if len(components) == 2:
+		minutes = int(components[0])
+		seconds = float(components[1])
+		return 60*minutes + seconds
+		
+	if value.endswith('ms'):
+		return float(value[:-2])/1000
+	if value.endswith('s'):
+		return float(value[:-1])
+	if value.endswith('min'):
+		return 60*float(value[:-3])
+	if value.endswith('h'):
+		return 3600*float(value[:-1])
+	return float(value)
+
+def interpolate(fromValue, toValue, t):
+	if isinstance(fromValue, Color):
+		return interpolateColor(fromValue, toValue, t)
+	else:
+		return interpolateValueArray(fromValue, toValue, t)
+
+def interpolateColor(fromColor, toColor, t):
+	r = interpolateSingleValue(fromColor.red, toColor.red, t)
+	g = interpolateSingleValue(fromColor.green, toColor.green, t)
+	b = interpolateSingleValue(fromColor.blue, toColor.blue, t)
+	a = interpolateSingleValue(fromColor.alpha, toColor.alpha, t)
+	return Color(r, g, b, a)
 
 def interpolateValueArray(fromArr, toArr, t):
 	retArr = []
